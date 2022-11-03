15d3c67e90a5ddfaba6a69927856a6d1946178ad
kelwinap <kelwinarthur8@gmail.com>
2022-11-03 09:55:18 -0300

    add get contracts route

--- src/controllers/contracts.js
+++ src/controllers/contracts.js
@@ -1,3 +1,7 @@
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+const TERMINATED = "terminated";
+
 const getContract = async (req, res) => {
   const { Contract } = req.app.get("models");
   const { id } = req.params;
@@ -12,6 +16,20 @@ const getContract = async (req, res) => {
   res.json(contract);
 };
 
+const getContracts = async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const profileId = req.profile.id;
+
+  const contracts = await Contract.findAll({
+    where: { ClientId: profileId, status: { [Op.not]: TERMINATED } },
+  });
+
+  if (!contracts) return res.status(404).end();
+
+  res.json(contracts);
+};
+
 module.exports = {
   getContract,
+  getContracts,
 };
--- src/routes/contracts.js
+++ src/routes/contracts.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getProfile } = require("../middleware/getProfile");
-const { getContract } = require("../controllers/contracts");
+const { getContract, getContracts } = require("../controllers/contracts");
 
 router.get("/:id", getProfile, getContract);
+router.get("/", getProfile, getContracts);
 
 module.exports = router;
